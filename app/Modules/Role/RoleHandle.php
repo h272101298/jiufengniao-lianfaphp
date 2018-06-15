2804e9207ace294748c2d654513787ea7b94a62c
ZENG <zeng@ZENGs-iMac.local>
2018-06-15 18:04:09 +0800

    fix

---
+++ app/Modules/Role/RoleHandle.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Modules\Role;
+/**
+ * Created by PhpStorm.
+ * User: zeng
+ * Date: 2018/6/15
+ * Time: 下午4:41
+ */
+trait RoleHandle
+{
+    public function getUserRole($user_id)
+    {
+
+    }
+    public function getUserPermissions($user_id)
+    {
+
+    }
+    public function getRoles()
+    {
+
+    }
+    public function getRolePermissions($role_id)
+    {
+
+    }
+    public function getPermissions()
+    {
+
+    }
+    public function getPermission($id)
+    {
+
+    }
+    public function delPermission()
+    {
+
+    }
+    public function createRole()
+    {
+
+    }
+    public function delRole($id)
+    {
+
+    }
+
+}
