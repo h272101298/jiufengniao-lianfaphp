c9b7b42a94a25f6cd2f3cbf18c2c3c607e81fa9d
ZENG <zeng@ZENGs-iMac.local>
2018-07-20 18:16:13 +0800

    fix

--- app/Modules/Bargain/BargainHandle.php
+++ app/Modules/Bargain/BargainHandle.php
@@ -64,10 +64,12 @@ trait BargainHandle
             return [];
         }
         foreach ($promotions as $promotion){
-            $promotion->product = Product::find($promotion->product_id);
+            $product = Product::find($promotion->product_id);
             if (!empty($product)){
                 unset($product->detail);
             }
+            $promotion->product = $product;
+
             $promotion->stock = Stock::find($promotion->stock_id);
             if ($time){
                 $promotion->start = date('Y-m-d H:i:s',$promotion->start);
