2d3bbabecc14b3547bfd788aaebf2953d09e6d22
DevZeng <zengshungeng@live.com>
2018-07-23 10:46:26 +0800

    fiux

--- app/Http/Controllers/V2/BargainController.php
+++ app/Http/Controllers/V2/BargainController.php
@@ -149,9 +149,9 @@ class BargainController extends Controller
         return jsonResponse([
             'msg'=>'ok',
             'data'=>[
-                'price'=>$promotion->origin_price-$price,
-                'count'=>$count,
-                'bargain_price'=>$price
+                'price'=>sprintf('%.2f',$promotion->origin_price-$price),
+                'count'=>$count+1,
+                'bargain_price'=>sprintf('%.2f',$price)
             ]
         ]);
 
