95c45a0b742b7d872f7f39f2c97c012a7b9eabe5
ZENG <zeng@ZENGs-iMac.local>
2018-11-14 18:06:03 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::options('{all}',function (){return jsonResponse(['msg'=>'ok']);})->middle
 //Route::options('/{all}',function (){return jsonResponse(['msg'=>'ok']);})->middleware('cross');
 //Route::options('v1/{all}',function (){jsonResponse(['msg'=>'ok']);})->middleware('cross');
 Route::get('test',function (){
-    return strtotime(date('Y-1-1'));
+    dd(\Illuminate\Support\Facades\Auth::check());
 });
 Route::post('test2','V1\SystemController@test');
 Route::group(['prefix'=>'v1','middleware'=>'cross'],function (){
