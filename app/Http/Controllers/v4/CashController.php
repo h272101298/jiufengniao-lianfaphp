f574902558d63e30ccea2a58f24a30d90624b280
h272101298 <272101298@qq.com>
2020-03-24 10:45:20 +0800

    test

--- app/Http/Controllers/v4/CashController.php
+++ app/Http/Controllers/v4/CashController.php
@@ -26,7 +26,7 @@ class CashController extends Controller
             'created_at'=>date('Y:m:d H:i:s',time()),
             'update_at'=>date('Y:m:d H:i:s',time())
         ];
-
+        dd($res);
         if($res['result_code'] == "SUCCESS"){
             $data['remarks']= '提现成功';
         }elseif($res['result_code'] == "FAIL"){
@@ -34,7 +34,7 @@ class CashController extends Controller
         }else{
             $data['remarks']=$res['msg'];
         }
-        dd($data);
+
         $msg=$this->handle->saveCashList($data);
         dd($msg);
 
