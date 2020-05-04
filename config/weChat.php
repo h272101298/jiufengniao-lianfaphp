1f94559f60d1a129c472fa2594ce9ad77617254a
h272101298 <272101298@qq.com>
2020-05-04 10:08:35 +0800

    321

--- config/weChat.php
+++ config/weChat.php
@@ -6,10 +6,10 @@
  * Time: 上午10:47
  */
 return [
-    'appId'=>'wx5671bd47b7d2166c',
-    'appSecret'=>'4b213482683bc98f412d2f6e37494070',
-    'mchId'=>'1507375401',
-    'appKey'=>'babihu20180327geckopaymentsecret',
-    'notifyUrl'=>'https://ddf.com',
-    'codeRequired'=>'code不能为空！'
+    'appId'=>'',
+    'appSecret'=>'',
+    'mchId'=>'',
+    'appKey'=>'',
+    'notifyUrl'=>'',
+    'codeRequired'=>'！'
 ];
