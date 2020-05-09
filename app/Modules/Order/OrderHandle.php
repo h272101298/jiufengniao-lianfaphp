28c550e935abffe9679e87a4aa5ad56cfc6fed29
h272101298 <272101298@qq.com>
2020-05-09 18:16:21 +0800

    123

--- app/Modules/Order/OrderHandle.php
+++ app/Modules/Order/OrderHandle.php
@@ -388,12 +388,13 @@ trait OrderHandle
     {
         $config = TxConfig::first();
         $refuse = Refuse::find($id);
-        $wxpay = getWxPay();
+        $data['return_code']='SUCCESS';
+        /*$wxpay = getWxPay();
         $path = base_path().'/public/';
         $order = Order::find($refuse->order_id);
         $total_fee = Order::where('group_number','=',$order->group_number)->sum('price');
         $data = $wxpay->refund($order->transaction_id,$order->number,$total_fee*100,$order->price*100,$config->mch_id,$path.$config->ssl_cert,
-            $path.$config->ssl_key);
+            $path.$config->ssl_key);*/
         if ($data['return_code']=='FAIL'){
             $refuse->state = 3;
 //            $refuse->remark = json_encode($data);
